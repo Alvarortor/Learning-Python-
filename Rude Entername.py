683e72990cb373513cbe2d0ce04fa23a6c82c23c
Alvarortor <95102990+Alvarortor@users.noreply.github.com>
2021-11-26 13:55:34 -0600

    Add files via upload

---
+++ Rude Entername.py
@@ -0,0 +1,12 @@
+name = input("Your name is: " )
+Q1 = input("So, your name is " + name + " correct?: ")
+if (Q1) == ("Yes") or Q1 == ("yes"):
+    Q1 = print("There is no fucking way something as stupid as " +name+ " is your name.")
+if Q1 == ("no") or Q1 == ("No"): 
+    print("I cant believe you're so stupid you dont know your name. How do you not know your name? Are you actually stupid or is it method acting?")
+name2 = input( "Try again dumbass, what's your name?: ")
+if name == name2:
+    print(" Oh wow, so you weren't pretending? " +name+ " really is your name? Pathetic.")
+else: 
+    print("You know, in ancient chinese "+name2+ " means failed abortion, so have fun with that.")
+input("Press Enter so that I don't have to see you anymore")
